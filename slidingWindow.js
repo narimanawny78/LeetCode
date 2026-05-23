e323f4465aa079732a48142af1117a393b517c58
narriman <narimanamer805@gmail.com>
2026-05-23 23:59:37 +0300

    adding Sliding optimized solution  solution

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,18 +1,40 @@
 //brute force solution
+// var maxSlidingWindow = function(nums, k) {
+//     let result = [];
+//     let n = nums.length;
+
+//     for(let i =0 ; i <= n - k ; i++ ){
+//         let max = nums[i];
+//         for(let j = 1; j < k ; j++){
+//             if(nums[i + j] > max){
+//                 max = nums[i + j];
+//             }
+//         }
+//             result.push(max);
+//     }
+//     return result;
+// }
+
+//These Solution gives => Time exceeded
+
+
+// The Optimized Solution
+
 var maxSlidingWindow = function(nums, k) {
     let result = [];
-    let n = nums.length;
+    let deque = [];
 
-    for(let i =0 ; i <= n - k ; i++ ){
-        let max = nums[i];
-        for(let j = 1; j < k ; j++){
-            if(nums[i + j] > max){
-                max = nums[i + j];
-            }
+    for(let i = 0 ; i < nums.length ; i++ ){
+        if(deque.length > 0 && deque[0] <= i - k){
+            deque.shift();
         }
-            result.push(max);
+        while(deque.length > 0 && nums[deque[deque.length-1]] < nums[i]){
+            deque.pop();
+        }
+        deque.push(i);
+        if(i >= k-1){
+            result.push(nums[deque[0]]);
+        }
     }
     return result;
 }
-
-//These Solution gives => Time exceeded
