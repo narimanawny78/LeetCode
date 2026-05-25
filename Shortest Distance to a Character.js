e6ad4782ffc7bd041d6134eb7ae99765ec0f399e
narriman <narimanamer805@gmail.com>
2026-05-25 16:14:22 +0300

    adding Shortest Distance to a Character  solution

---
+++ Shortest Distance to a Character.js
@@ -0,0 +1,27 @@
+var shortestToChar = function(s, c) {
+    let answer = [];
+    let lastSeen = -s.length;
+
+    for(let i = 0 ; i < s.length ; i++){
+        if(s[i] === c){
+            lastSeen = i;
+        }
+       let distance = Math.abs(i - lastSeen);
+        answer.push(distance);
+    }
+    for(let j = s.length -1 ; j >= 0 ; j--){
+        if(s[j] === c){
+            lastSeen = j;
+        }
+       let newdistance = Math.abs(j - lastSeen);
+        answer[j] = Math.min(answer[j] , newdistance);
+
+    }
+    return answer;
+};
+
+console.log(shortestToChar("loveleetcode", "e"));
+
+//[3 , 2 , 1, 0 , 1, 0, 0 ,1 ,2 ,2, 1 ,0]
+//[k , k , k, 0 , 1, 0, 0, 1, 2, k, k, 0]
+//[12, 13, 14, 0, 1, 0, 0, 1, 2, 3, 4, 0]
