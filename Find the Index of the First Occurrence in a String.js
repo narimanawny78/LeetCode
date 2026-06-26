ad1574c6030d1b263a14912b1713cceb4aa8d989
narriman <narimanamer805@gmail.com>
2026-06-26 23:58:45 +0300

    adding Find the Index of the First Occurrence in a String solution without bultin functions

--- Find the Index of the First Occurrence in a String.js
+++ Find the Index of the First Occurrence in a String.js
@@ -1,3 +1,28 @@
+// var strStr = function(haystack, needle) {
+//     return haystack.indexOf(needle);
+// };
+
+//Another Soltution without Bult-in functions
+
+
+"sadbutsad"
+
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    if (needle.length > haystack.length && needle === "") return -1;
+
+    for(let i = 0 ; i <= haystack.length - needle.length; i++ ){
+        let flag = true;
+        for(let j = 0 ; j < needle.length ; j++){
+            if(haystack[i + j] !== needle[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            return i;
+        }
+    }
+    return -1;
 };
+
+console.log(strStr("sadbutsad", "sad"));
